6fcb7ced1e7ea17ada00756b436d51f977aa2287
Saida <saidasaifutdinova@yandex.ru>
2025-04-17 23:47:34 +0300

    Исправлен код-стиль-6.

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,11 @@ import service.TaskManager;
 
 public class Main {
     public static void main(String[] args) {
-        /*TaskManager taskManager = Managers.getDefault(Managers.getDefaultHistory());
+        TaskManager taskManager = Managers.getDefault(Managers.getDefaultHistory());
         taskManager.createTask(new Task("Имя1", "Описание1", Status.NEW));
         taskManager.createTask(new Task("Имя2", "Описание2", Status.NEW));
         taskManager.createTask(new Epic("Имя", "Описание", Status.NEW));
         taskManager.createSubtask(new SubTask("Имя2", "Описание2", Status.NEW, 1));
-        taskManager.createSubtask(new SubTask("Имя", "Описание", Status.NEW, 1));*/
+        taskManager.createSubtask(new SubTask("Имя", "Описание", Status.NEW, 1));
     }
 }
